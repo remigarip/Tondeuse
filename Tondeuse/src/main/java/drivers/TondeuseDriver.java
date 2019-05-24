a1353dd9b8b57f8ec4f519ea7641df87b7923ce1
Rémi Garip <garipremi@gmail.com>
2019-05-24 09:24:36 +0200

    Correction URL

--- Tondeuse/src/main/java/drivers/TondeuseDriver.java
+++ Tondeuse/src/main/java/drivers/TondeuseDriver.java
@@ -6,7 +6,8 @@ public class TondeuseDriver {
 	
 	public static void main(String[] args) {
 		final TondeuseService service = new TondeuseService();
-		service.executionProgramme("D:/Profiles/regarip/Desktop/Temp/Tondeuse/src/main/resources/input.txt");
+		// Modifier l'URL ici
+		service.executionProgramme("D:/monDossier/Tondeuse/src/main/resources/input.txt"");
 	}
 	
 
